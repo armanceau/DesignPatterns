a756a0532b140fccdba7dfff5561b6abaa9ee91e
Arthur Manceau <102350268+armanceau2@users.noreply.github.com>
2023-10-24 17:06:33 +0200

    add

--- Classes/main.java
+++ Classes/main.java
@@ -16,50 +16,88 @@ public class main {
         cours coursJava = cours.creerCours(1, "Java");
         cours coursPHP = cours.creerCours(2, "PHP");
         cours coursDesignPatterns = cours.creerCours(3, "Design patterns");
-        cours coursJS = cours.creerCours(3, "JS");
-        
-        
+         
         // Ajout des cours au parcours
         parcours.ParcoursDev.add(coursJava);
         parcours.ParcoursDev.add(coursPHP);
         parcours.ParcoursDev.add(coursDesignPatterns);
-        
+
+        System.out.println();
+        System.out.println("---------------------------------------------------");
+        System.out.println();
+        System.out.println("MyEfrei");
+        System.out.println();
         
         boolean continuerNavigation = false;
         while(!continuerNavigation) {
-            
-
+            System.out.println();
+            System.out.println("---------------------------------------------------");
+            System.out.println();
             System.out.println("Voici la liste des possibilités que vous pouvez réaliser : ");
+            System.out.println();
             System.out.println("1- Voir les parcours disponibles de l'EFREI");
             System.out.println("2- Ajouter un cours au parcours choisi");
             System.out.println("3- Voir la liste des cours du parcours choisi");
+            System.out.println();
             System.out.println("Quelle option voulez-vous réaliser ? (1, 2, 3)");
-            String option = scan.nextLine();
-            Integer intOption = Integer.parseInt(option);
             
+            Integer option = scan.nextInt();
+            scan.nextLine();
 
-
-            switch (intOption){
+            switch (option){
                 case 1 : 
+                    System.out.println();
+                    System.out.println("---------------------------------------------------");
+                    System.out.println();
                     System.out.println("Voici la liste des parcours présents à l'EFREI : ");
-                    System.out.println("Parcours 1 : Parcours de developpement.");
-                    System.out.println("Parcours 2 : Parcours de cyber securite.");
+                    System.out.println();
+                    System.out.println("- Parcours 1 : Parcours de developpement.");
+                    System.out.println("- Parcours 2 : Parcours de cyber securite.");
                 break; 
 
                 case 2: 
-                    parcours.AjouterCours();
+                    
+                    boolean numAjoutParcoursValide = false;
+                    System.out.println("A quel parcours voulez-vous ajouter un cours ? (1,2)");
+                    Integer parcoursAjout = scan.nextInt();
+                    while(!numAjoutParcoursValide){
+                        switch(parcoursAjout){
+                            case 1 : 
+                                System.out.println("Insérez le nom du cours à ajouter : ");
+                                String nomCours = scan.nextLine();
+                                cours CoursAjout = cours.creerCours(parcours.ParcoursDev.size()+1, nomCours);
+                                parcours.ParcoursDev.add(CoursAjout);
+                                numAjoutParcoursValide = true;
+                            break;
+
+                            case 2 : 
+                                System.out.println("Insérez le nom du cours à ajouter : ");
+                                nomCours = scan.nextLine();
+                                CoursAjout = cours.creerCours(parcours.ParcoursCyber.size()+1, nomCours);
+                                parcours.ParcoursCyber.add(CoursAjout);
+                                numAjoutParcoursValide = true;
+                            break;
+
+                            default : 
+                                System.out.println("Désolé je n'ai pas compris");
+                            break;
+                        }
+                    }
+                    
+
                 break;
 
                 case 3 : 
-                    
-
                     boolean numParcoursValide = false;
                     while(!numParcoursValide){
-                        System.out.println("De quel parcours souhiatez-vous voir les cours ? (1, 2)");
+                        System.out.println();
+                        System.out.println("---------------------------------------------------");
+                        System.out.println();
+                        System.out.println("De quel parcours souhaitez-vous voir les cours ? (1, 2)");
                         String numParcours = scan.nextLine();
 
                         if(numParcours.equals("1")){
-                            System.out.println("Voici la liste des cours présents dans le parcours developpement : ");
+                            System.out.println("Voici la liste des cours présents dans le parcours de developpement : ");
                             // Affichage des cours du parcours : parcoursDev
                             for (cours cours : parcours.ParcoursDev){
                                 System.out.println("-" + cours.getNom());
@@ -78,14 +116,14 @@ public class main {
                             System.out.println("Désolé, je n'ai pas compris votre demande. Assurez-vous de bien écrire 1 ou 2.");
                         }
                     }
-                    scan.close(); 
-                    continuerNavigation = true;
+                    
+                    // continuerNavigation = true;
+                break;
+                default:
+                System.out.println("Choix incorrect");
                 break;
             }
-            
-            
-        }
-        
+        }  
          scan.close(); 
     }
 }
--- Classes/parcours.java
+++ Classes/parcours.java
@@ -1,20 +1,9 @@
 package Classes;
 
-import java.util.Scanner;
 import java.util.ArrayList;
 
 public class parcours {
     // Déclaration d un nouveau parcours 
     static ArrayList<cours> ParcoursDev = new ArrayList<>();
     static ArrayList<cours> ParcoursCyber = new ArrayList<>();
-
-    public static void AjouterCours() {
-        Scanner scan2 = new Scanner(System.in);
-        System.out.println("Insérez le nom du cours à ajouter : ");
-        String nomCours = scan2.nextLine();
-        cours CoursAjout = cours.creerCours(5, nomCours);
-        ParcoursDev.add(CoursAjout);
-        scan2.close();
-    }
-
 }
